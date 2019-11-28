c7dbb1ca86a2755e01a16885c7b6825dbe1d6e79
League Student <league@iMac-11.attlocal.net>
2019-11-27 16:58:52 -0800

    Houses IP :smile:

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,75 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
+	int x = 700;
+	int y = 500;
+		
+	Robot rob = new Robot();
+	rob.setX(x);
+	rob.setY(y);
+	rob.setSpeed(50);
+	rob.penDown();
+	
+	drawHouse(rob, "large", "purple");
+	drawHouse(rob, "small", "grey");
+	drawHouse(rob, "medium", "salmon");
+	drawHouse(rob, "medium", "blue");
+	drawHouse(rob, "large", "pink");
+	drawHouse(rob, "small", "purple");
+	drawHouse(rob, "medium", "salmon");
+	drawHouse(rob, "large", "grey");
+	drawHouse(rob, "small", "blue");
+	drawHouse(rob, "small", "pink");
+	}
+	
+	public void drawHouse(Robot bob, int height) {
+		bob.move(height);
+		bob.turn(-90);
+		bob.move(25);
+		bob.turn(-90);
+		bob.move(height);
+		bob.setPenColor(0,200,0);
+		bob.turn(90);
+		bob.move(15);
+		bob.turn(90);
+	}
+	
+	public void drawHouse(Robot bob, String height, String color) {
+		
+		if (color.equals("purple")) {
+			bob.setPenColor(204, 153, 255);
+		}
+		
+		else if (color.equals("blue")) {
+			bob.setPenColor(153, 204, 255);
+		}
+		
+		else if (color.equals("white")) {
+			bob.setPenColor(255, 294, 229);		
+		}
+		
+		else if (color.equals("salmon")) {
+			bob.setPenColor(255, 153, 153);
+		}
+		
+		else if (color.equals("grey")) {
+			bob.setPenColor(192, 192, 192);
+		}
+		
+		if (height.equals("small")) {
+			drawHouse(bob, 60);
+		}
+		
+		else if (height.equals("medium")) {
+			drawHouse(bob, 120);
+		}
+		
+		else if (height.equals("large")) {
+			drawHouse(bob, 250);
+		}
 		
 	}
 }
