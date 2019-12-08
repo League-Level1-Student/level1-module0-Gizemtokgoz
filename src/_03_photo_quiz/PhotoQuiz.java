8f9d57b0751890740acc168b5436e8b654257955
League Student <league@iMac-13.attlocal.net>
2019-12-08 12:59:11 -0800

    PhotoQuiz, DrumKit :smile:

--- src/_03_photo_quiz/PhotoQuiz.java
+++ src/_03_photo_quiz/PhotoQuiz.java
@@ -26,11 +26,11 @@ public class PhotoQuiz {
 		// 1. find an image on the internet, and put its URL in a String
 		// variable (from your browser, right click on the image, and select
 		// “Copy Image Address”)
-		String peppa = "https://www.myepl.org/wp-content/uploads/2019/09/Peppa.jpg";
+		String george = "https://patch.com/img/cdn/users/83443/2015/01/raw/20150154b83821dbaa3.png";
 		// 2. create a variable of type "Component" that will hold your image
 		Component com;
 		// 3. use the "createImage()" method below to initialize your Component
-		com = createImage(peppa);
+		com = createImage(george);
 		// 4. add the image to the quiz window
 		quizWindow.add(com);
 		// 5. call the pack() method on the quiz window
@@ -38,27 +38,35 @@ public class PhotoQuiz {
 		// 6. ask a question that relates to the image
 		String answer = JOptionPane.showInputDialog("Who is this?");
 		// 7. print "CORRECT" if the user gave the right answer
-		if () {
-			
+		if (answer.equals("curious george")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
 		}
 		// 8. print "INCORRECT" if the answer is wrong
-		else if () {
-			
+		else {
+			JOptionPane.showMessageDialog(null, "You are incorrect :(");
 		}
 		// 9. remove the component from the quiz window (you may not see the
 		// effect of this until step 12)
 		quizWindow.remove(com);
 		// 10. find another image and create it (might take more than one line
 		// of code)
-
+		String shrek = "https://vignette.wikia.nocookie.net/cinemasins/images/9/98/Shrek.png/revision/latest?cb=20170119184541";
+		Component add;
+		add = createImage(shrek);
 		// 11. add the second image to the quiz window
-
+		quizWindow.add(add);
 		// 12. pack the quiz window
-
+		quizWindow.pack();
 		// 13. ask another question
-
+		String ans = JOptionPane.showInputDialog("Who is this?");
 		// 14+ check answer, say if correct or incorrect, etc.
+		if (ans.equals("shrek")) {
+			JOptionPane.showMessageDialog(null, "You are correct!");
+		}
 
+		else {
+			JOptionPane.showMessageDialog(null, "You are incorrect :(");
+		}
 	}
 
 	private Component createImage(String imageUrl) throws MalformedURLException {
--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -21,49 +21,54 @@ import javax.swing.JPanel;
 public class DrumKit implements MouseListener {
 
 	JLabel drumLabelWithImage;
+	JLabel cymbalLabelWithImage;
 
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
-
+		JFrame kit = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
-
+		kit.setVisible(true);
+		kit.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. Set the size of the frame
-
+		kit.setSize(500, 500);
 		// 4. Set the title of the frame
-
+		kit.setTitle("drums");
 		// 5. Make a JPanel variable and initialize it using "new JPanel().
-
+		JPanel pan = new JPanel();
 		// 6. Add the panel to the frame. (The panel is invisible.)
-
+		kit.add(pan);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
 
 		// 8. Put the name of your image file in a String variable.
-
+		String drum = "drum.jpg";
+		String cymbal = "cymbal.jpg";
 		// 9. Edit the next line to use your String variable
-		// drumLabelWithImage = createLabelImage(drumImageString);
-
+		drumLabelWithImage = createLabelImage(drum);
+		cymbalLabelWithImage = createLabelImage(cymbal);
 		// 10. Add the image to the panel
-
+		pan.add(drumLabelWithImage);
+		pan.add(cymbalLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+		pan.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+		kit.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+		drumLabelWithImage.addMouseListener(this);
+		cymbalLabelWithImage.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
-
+		// done
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
-
-		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
+		System.out.println("mouse clicked");
+		JLabel clicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
 														// clicked on
 
@@ -72,10 +77,17 @@ public class DrumKit implements MouseListener {
 		// leagueofamazing/code4life.
 
 		// 16. If they clicked on the drumImage...
-
+		if (clicked == drumLabelWithImage) {
 		// 17. ...use the playSound method to play a drum sound. Test to see if
 		// it works
-
+			playSound("drum.wav");
+			
+		if (clicked == cymbalLabelWithImage) {
+			playSound("cymbal.wav");
+			
+			
+			}
+		}
 	}
 
 	private JLabel createLabelImage(String fileName) throws MalformedURLException {
