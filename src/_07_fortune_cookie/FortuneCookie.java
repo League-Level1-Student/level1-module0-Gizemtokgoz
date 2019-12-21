b53d7d21aa530735ea9b72e377e6d64c2f89462f
Gizemtokgoz <48003456+Gizemtokgoz@users.noreply.github.com>
2019-12-21 12:28:33 -0800

    FortuneCookie IP, JackInTheBox IP :confused:

--- src/_07_fortune_cookie/FortuneCookie.java
+++ src/_07_fortune_cookie/FortuneCookie.java
@@ -1,5 +1,29 @@
 package _07_fortune_cookie;
 
-public class FortuneCookie {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookie implements ActionListener {
+	public void showButton() {
+		System.out.println("Button clicked");
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JButton button = new JButton();
+		button.setSize(50, 50);
+		frame.add(button);
+		frame.pack();
+		button.addActionListener(this);
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Woohoo!");
+		int rand = new Random().nextInt(5);
+	}
 
 }
